86b8dfe66d59d196bf8249e47b3314c435899c82
aronbarbosag <aronfbarbosa@gmail.com>
2024-11-07 22:31:21 -0300

    funcionalidades basicas

--- app/urls.py
+++ app/urls.py
@@ -2,4 +2,12 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("", views.index, name="home"),
+    path("sobre-nos/", views.sobre, name="sobre"),
+    path("transparencia/", views.transparencia, name="transparencia"),
+    path("voluntariado/", views.voluntariado, name="voluntariado"),
+    path("contato/", views.contato, name="contato"),
+    path("doacao/", views.doacao, name="doacao"),
+    path("eventos/", views.eventos, name="eventos"),
+    path("area-restrita/", views.area_restrita, name="area-restrita"),
+
 ]
--- app/views.py
+++ app/views.py
@@ -5,3 +5,31 @@ from django.shortcuts import render, redirect
 
 def index(request):
     return render(request, "app/index.html", {"idBody": " "})
+
+
+def sobre(request):
+    return render(request, "app/sobre.html", {"idBody": "sobre"})
+
+
+def transparencia(request):
+    return render(request, "app/transparencia.html", {"idBody": "transparencia"})
+
+
+def voluntariado(request):
+    return render(request, "app/voluntariado.html", {"idBody": "voluntariado"})
+
+
+def contato(request):
+    return render(request, "app/contato.html", {"idBody": "contato"})
+
+
+def doacao(request):
+    return render(request, "app/doacao.html", {"idBody": "doacao"})
+
+
+def eventos(request):
+    return render(request, "app/eventos.html", {"idBody": "eventos"})
+
+
+def area_restrita(request):
+    return render(request, "app/area-restrita.html", {"idBody": "area-restrita"})
