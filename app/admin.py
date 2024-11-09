b26495aea0a5eb0be9ba349bef95c926c4bc77a1
aronbarbosag <aronfbarbosa@gmail.com>
2024-11-09 19:24:55 -0300

    tabela atividade e participacao

--- app/admin.py
+++ app/admin.py
@@ -1,12 +1,33 @@
 from django.contrib import admin
-from .models import Crianca
+from .models import Crianca, Atividade, Participacao
 
 
 class CriancaAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'data_de_nascimento',
-                    'tamanho_camiseta', 'escola', 'turno', 'telefone_responsavel')
+    list_display = ('nome', 'idade_calculada',
+                    'escola',  'turno', 'rua', 'telefone_responsavel')
     search_fields = ('nome',)
-    list_filter = ('data_de_nascimento',)
+
+    list_filter = ('nome', 'data_de_nascimento')
+
+    list_per_page = (10)
+
+    def idade_calculada(self, obj):
+        return obj.idade  # Acessa a propriedade `idade` do modelo
+
+    # Nome que aparecerá no Django Admin
+    idade_calculada.short_description = 'Idade'
+
+
+class AtividadeAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+
+
+class ParticipacaoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'atividade', 'data_inicio', 'data_fim', 'status')
+    list_filter = ('atividade', 'status')
+    list_per_page = (10)
 
 
 admin.site.register(Crianca, CriancaAdmin)
+admin.site.register(Atividade, AtividadeAdmin)
+admin.site.register(Participacao, ParticipacaoAdmin)
